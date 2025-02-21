565a607ac631f3a3b744c75072d0e2048000feac
Purushottam Patle <patlep08@gmail.com>
2025-02-21 13:02:26 +0530

    ss

--- backend/models/ChannelModal.js
+++ backend/models/ChannelModal.js
@@ -6,40 +6,40 @@ const channelSchema = new mongoose.Schema({
         required: true,
     },
     members: [{
-            type: mongoose.Schema.Types.ObjectId,
-            ref: "Users",
-            required: true
-        }],
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Users",
+        required: true
+    }],
     admin: [{
-            type: mongoose.Schema.Types.ObjectId,
-            ref: "Users",
-            required: true
-        }],
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Users",
+        required: true
+    }],
     messages: [{
-            type: mongoose.Schema.Types.ObjectId,
-            ref: "Messages",
-            required: false
-        }],
-        createdAt: {
-            type: Date,
-            default: Date.now,
-        },
-        updatedAt: {
-            type: Date,
-            default: Date.now,
-        },
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Messages",
+        required: false
+    }],
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    },
+    updatedAt: {
+        type: Date,
+        default: Date.now,
+    },
 });
 
-channelSchema.pre("save", function(next){
+channelSchema.pre("save", function (next) {
     this.updatedAt = Date.now();
     next();
 })
 
-channelSchema.pre("findOneAndUpdate", function(next){
-    this.set({updatedAt:Date.now()});
+channelSchema.pre("findOneAndUpdate", function (next) {
+    this.set({ updatedAt: Date.now() });
     next();
 })
 
- const Channel=  mongoose.model("Channels", channelSchema);
+const Channel = mongoose.model("Channels", channelSchema);
 
- export default Channel;
+export default Channel;
